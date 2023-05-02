bb03423620cdf04038a4a749f32c8f2614598d2c
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2023-05-02 15:52:10 -0700

    Update index.js

--- screens/index.js
+++ screens/index.js
@@ -10,11 +10,10 @@ import { useState, useEffect, useContext } from "react";
 import { useNavigation } from "@react-navigation/native";
 import PageContainer from "../components/pageContainer";
 import { RestaurantCard } from "../components/cards";
+import { LocationModalView } from "../components/modal";
 import { getBreakPoint } from "../utils/screen";
 import { popularPicks } from "../api/get";
 import { addressDetailsContext } from "../contexts/AddressContext";
-import { LocationModalView } from "../components/modal";
-import FoodTypes from "./foodTypes";
 
 const Index = () => {
   const navigation = useNavigation();
@@ -58,136 +57,28 @@ const Index = () => {
         </>
       ) : (
         <>
-          <>
-            <LocationModalView
-              visible={visible}
-              setVisible={setVisible}
-              setPopularRestaurants={setPopularRestaurants}
-            />
-            <View style={tailwind("flex flex-row justify-between")}>
-              <View>
-                <Text style={tailwind("text-2xl font-bold")}>
-                  Give yourself a treat!🥘
-                </Text>
-              </View>
-              <View style={tailwind("flex flex-row")}>
-                <TouchableOpacity
-                  onPress={() => navigation.navigate("Checkout")}
-                >
-                  <Image
-                    style={tailwind("w-9 h-9 mr-4")}
-                    resizeMode="contain"
-                    source={require("../assets/icons/black/cart.png")}
-                  />
-                </TouchableOpacity>
-                <Image
-                  style={[tailwind("w-9 h-9"), { borderRadius: 20 }]}
-                  resizeMode="contain"
-                  source={{ uri: faker.image.avatar() }}
-                />
-              </View>
-            </View>
-            <View style={tailwind("flex flex-row")}>
-              <TouchableOpacity
-                // Shows up the modal for the location setup when clicked on the location button
-                onPress={() => setVisible(true)}
-                style={tailwind("flex-row")}
-              >
-                <Image
-                  style={tailwind("w-4 h-4")}
-                  resizeMode="contain"
-                  source={require("../assets/icons/black/location.png")}
-                />
-                <Text style={tailwind("font-light ml-2")}>
-                  {/* {address?.address?.address1} */}
-                  {address[0].address.address1}
-                </Text>
-              </TouchableOpacity>
-            </View>
-            <View style={tailwind("flex flex-row items-center")}>
-              {!foodTypeScreen ? (
-                <Image
-                  style={tailwind("w-5 h-5")}
-                  resizeMode="contain"
-                  source={require("../assets/icons/black/search.png")}
-                />
-              ) : (
-                <TouchableOpacity onPress={() => showFoodTypeScreen(false)}>
-                  <Image
-                    style={tailwind("w-5 h-5")}
-                    resizeMode="contain"
-                    source={require("../assets/icons/black/back.png")}
-                  />
-                </TouchableOpacity>
-              )}
-
-              <TextInput
-                ref={searchBarRef}
-                placeholder="What would you like to eat?"
-                onFocus={() => showFoodTypeScreen(true)}
-                style={{
-                  height: 40,
-                  margin: 12,
-                  borderLeftWidth: 1,
-                  padding: 10,
-                  width:
-                    Platform.OS === "web" ? window.width / 2 : window.width,
-                }}
-                onSubmitEditing={({ nativeEvent: { text } }) => {
-                  navigation.navigate("Search", {
-                    searchStr: text,
-                  });
-                }}
-              />
-            </View>
-          </>
-          <>
-            {foodTypeScreen ? (
-              <View ref={foodTypesRef}>
-                <FoodTypes closeFoodTypes={() => showFoodTypeScreen(false)} />
-              </View>
-            ) : (
-              <>
-                <View
-                  style={[
-                    tailwind("flex flex-row justify-between"),
-                    {
-                      alignItems: "center",
-                      alignContent: "center",
-                      paddingBottom: 15,
-                    },
-                  ]}
-                >
-                  <Text style={[tailwind("text-2xl font-bold")]}>
-                    Main Course
-                  </Text>
-                </View>
-                <>
-                  {popularRestaurants?.length != 0 ? (
-                    <FlatList
-                      data={popularRestaurants}
-                      renderItem={({ item }) => {
-                        return (
-                          <View style={[tailwind("flex flex-1 ")]}>
-                            <RestaurantCard
-                              style={tailwind("m-2")}
-                              title={item.title}
-                              image={item.image}
-                              rating={
-                                item.rating === null
-                                  ? "No Ratings Found"
-                                  : item.rating.toFixed(1)
-                              }
-                              onPress={() => {
-                                navigation.navigate("Menu", {
-                                  postmates: item.ids.postmates,
-                                  grubhub: item.ids.grubhub,
-                                  doordash: item.ids.doordash,
-                                });
-                              }}
-                            />
-                          </View>
-                        );
+          <Text style={[tailwind("text-2xl font-bold")]}>Main Course</Text>
+          {popularRestaurants?.length != 0 ? (
+            <FlatList
+              data={popularRestaurants}
+              renderItem={({ item }) => {
+                return (
+                  <View style={[tailwind("flex flex-1 ")]}>
+                    <RestaurantCard
+                      style={tailwind("m-2")}
+                      title={item.title}
+                      image={item.image}
+                      rating={
+                        item.rating === null
+                          ? "No Ratings Found"
+                          : item.rating.toFixed(1)
+                      }
+                      onPress={() => {
+                        navigation.navigate("Menu", {
+                          postmates: item.ids.postmates,
+                          grubhub: item.ids.grubhub,
+                          doordash: item.ids.doordash,
+                        });
                       }}
                     />
                   </View>
